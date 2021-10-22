42cf6755bb6de6cf2c545fca39cd4ba8bb3be009
Timothy Strzelecki <timstrze@gmail.com>
2021-10-22 11:33:39 -0400

    Home page updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
